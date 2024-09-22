fcfb8590f9b943cc37f73128fe8cce531973e154
tamim <abutalhatamim017@gmail.com>
2024-09-22 22:29:17 +0600

    thi is parsiyeli work

---
+++ js/home.js
@@ -0,0 +1,6 @@
+document.getElementById('btn-show-add-money').addEventListener('click',function(event){
+    event.preventDefault();
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    
+
+});
