23facb7c561129ff5358c6c7540cb075014d60c9
tamim <abutalhatamim017@gmail.com>
2024-09-22 21:56:25 +0600

    Home work is started

--- js/login.js
+++ js/login.js
@@ -1,9 +1,20 @@
-document.getElementById('button-login').addEventListener('click', function (event) {
-    event.preventDefault();  // Prevents the form from being submitted
+// document.getElementById('button-login').addEventListener('click', function (event) {
+//     event.preventDefault();  // Prevents the form from being submitted
 
-    console.log('login button clicked');  // Logs the button click
+//       // Logs the button click
 
-    // Step 3: Get the phone number input value correctly
+//     // Step 3: Get the phone number input value correctly
+//     const phoneNumber = document.getElementById('phone-number').value;
+//     const pinNumber = document.getElementById('pin-number').value;
+//     console.log(phoneNumber, pinNumber);  // Logs the actual phone number
+// });
+
+document.getElementById('button-login').addEventListener('click',function(event){
+    event.preventDefault();
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);  // Logs the actual phone number
-});
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber,pinNumber);
+    
+
+
+})
